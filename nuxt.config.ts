40d5efd49d1fbe24f41e219e1bcac9cd2b970d66
半花 <115675140+ChenXueXin-dev@users.noreply.github.com>
2024-07-17 20:29:21 +0800

    Add files via upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://nuxt.com/docs/api/configuration/nux-config
 export default defineNuxtConfig({
+  //开启 ssr服务器渲染，默认开启
+  ssr:true,
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  //开启调试工具
+  devtools: { enabled: true },
+  //应用模块
+  modules:['@vant/nuxt']
 })
